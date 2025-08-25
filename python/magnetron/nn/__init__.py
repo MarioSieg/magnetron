b05696d416e3e507741ed9ac8c872e0701ed1666
Mario Sieg <mario.sieg.64@gmail.com>
2025-08-25 15:18:22 +0000

    Update copyright

--- python/magnetron/nn/__init__.py
+++ python/magnetron/nn/__init__.py
@@ -1,10 +1,10 @@
 # +---------------------------------------------------------------------+
-# | (c) 2025 Mario Sieg <mario.sieg.64@gmail.com>                 |
+# | (c) 2025 Mario Sieg <mario.sieg.64@gmail.com>                       |
 # | Licensed under the Apache License, Version 2.0                      |
 # |                                                                     |
 # | Website : https://mariosieg.com                                     |
 # | GitHub  : https://github.com/MarioSieg                              |
-# | License : https://www.apache.org/licenses/LICENSE-2.0                |
+# | License : https://www.apache.org/licenses/LICENSE-2.0               |
 # +---------------------------------------------------------------------+
 
 from .activations import *
