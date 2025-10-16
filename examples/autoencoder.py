9f58cb7f53080bbc948014d55088853f08132cdf
Mario Sieg <mario.sieg.64@gmail.com>
2025-10-16 13:16:11 +0000

    Add AE example

---
+++ examples/autoencoder.py
@@ -0,0 +1,34 @@
+from magnetron import nn, optim, Tensor
+
+W, H = 32, 32
+x = Tensor.normal(1, 1, W, H)
+
+class AE(nn.Module):
+    def __init__(self, latent_dim: int = 64):
+        super().__init__()
+        self.latent_dim = latent_dim
+        self.encoder = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(W*H, latent_dim),
+            nn.ReLU(),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, W*H),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        y2 = self.decoder(self.encoder(x))
+        return y2.view(x.shape[0], 1, W, H)
+
+model = AE()
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+for step in range(1000):
+    optimizer.zero_grad()
+    y = model(x)
+    loss = criterion(y, x)
+    loss.backward()
+    optimizer.step()
+    if step % 100 == 0:
+        print(f'Epoch [{step}/1000] Loss: {loss.item():.6f}')
