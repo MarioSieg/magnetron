f17f5fa26f3f189473998b4098a478f08798d48f
Mario Sieg <mario.sieg.64@gmail.com>
2025-11-11 13:04:56 +0000

    Use fast math intrinsics

--- magnetron/cuda/mag_cuda_unary.cu
+++ magnetron/cuda/mag_cuda_unary.cu
@@ -14,32 +14,32 @@
 namespace mag {
     constexpr mag_e8m23_t INVSQRT2 = 0.707106781186547524400844362104849039284835937f /* 1/√2 */;
 
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_abs(mag_e8m23_t x) { return abs(x); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_abs(mag_e8m23_t x) { return fabs(x); }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sgn(mag_e8m23_t x) { return x > 0.f ? 1.f : x < 0.f ? -1.f : 0.f; }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_neg(mag_e8m23_t x) { return -x; }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_log(mag_e8m23_t x) { return logf(x); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_log(mag_e8m23_t x) { return __logf(x); }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sqr(mag_e8m23_t x) { return x*x; }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sqrt(mag_e8m23_t x) { return sqrtf(x); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sin(mag_e8m23_t x) { return sinf(x); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_cos(mag_e8m23_t x) { return cosf(x); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sin(mag_e8m23_t x) { return __sinf(x); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_cos(mag_e8m23_t x) { return __cosf(x); }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_step(mag_e8m23_t x) { return x > 0.f; }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_exp(mag_e8m23_t x) { return expf(x); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_exp(mag_e8m23_t x) { return __expf(x); }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_floor(mag_e8m23_t x) { return floorf(x); }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_ceil(mag_e8m23_t x) { return ceilf(x); }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_round(mag_e8m23_t x) { return nearbyintf(x); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_softmax(mag_e8m23_t x) { return exp(x); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_softmax_dv(mag_e8m23_t x) { return exp(x); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sigmoid(mag_e8m23_t x) { return 1.f/(1.f + expf(-x)); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sigmoid_dv(mag_e8m23_t x) { mag_e8m23_t sig = 1.f/(1.f + expf(-x)); return sig*(1.f-sig); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_softmax(mag_e8m23_t x) { return __expf(x); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_softmax_dv(mag_e8m23_t x) { return __expf(x); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sigmoid(mag_e8m23_t x) { return 1.f/(1.f + __expf(-x)); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_sigmoid_dv(mag_e8m23_t x) { mag_e8m23_t sig = 1.f/(1.f + __expf(-x)); return sig*(1.f-sig); }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_hard_sigmoid(mag_e8m23_t x) { return fminf(1.f, fmaxf(0.0f, (x + 3.0f)/6.0f)); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_silu(mag_e8m23_t x) { return x*(1.f/(1.f + expf(-x))); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_silu_dv(mag_e8m23_t x) { mag_e8m23_t sig = 1.f/(1.f + expf(-x)); return sig + x*sig; }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_tanh(mag_e8m23_t x) { return tanhf(x); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_tanh_dv(mag_e8m23_t x) { mag_e8m23_t th = tanhf(x); return 1.f - th*th; }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_silu(mag_e8m23_t x) { return x*(1.f/(1.f + __expf(-x))); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_silu_dv(mag_e8m23_t x) { mag_e8m23_t sig = 1.f/(1.f + __expf(-x)); return sig + x*sig; }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_tanh(mag_e8m23_t x) { return __tanhf(x); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_tanh_dv(mag_e8m23_t x) { mag_e8m23_t th = __tanhf(x); return 1.f - th*th; }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_relu(mag_e8m23_t x) { return fmax(0.f, x); }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_relu_dv(mag_e8m23_t x) { return x > 0.f ? 1.f : 0.f; }
     [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_gelu(mag_e8m23_t x) { return .5f*x*(1.f+erff(x*INVSQRT2)); }
-    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_gelu_dv(mag_e8m23_t x) { mag_e8m23_t th = tanhf(x); return .5f*(1.f + th) + .5f*x*(1.f - th*th); }
+    [[nodiscard]] __device__ __forceinline__ static mag_e8m23_t op_gelu_dv(mag_e8m23_t x) { mag_e8m23_t th = __tanhf(x); return .5f*(1.f + th) + .5f*x*(1.f - th*th); }
 
     template <mag_e8m23_t (&op)(mag_e8m23_t), typename T>
     __global__ static void unary_op_kernel(int n, T *o, const T *x) {
