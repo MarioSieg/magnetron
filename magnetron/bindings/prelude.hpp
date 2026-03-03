5fb98dea4b9b86dd7464613760c0f27016f4edbd
Mario Sieg <mario.sieg.64@gmail.com>
2026-03-03 16:58:31 +0000

    Make bindings target

---
+++ magnetron/bindings/prelude.hpp
@@ -0,0 +1,16 @@
+/*
+** +---------------------------------------------------------------------+
+** | (c) 2025 Mario Sieg <mario.sieg.64@gmail.com>                       |
+** | Licensed under the Apache License, Version 2.0                      |
+** |                                                                     |
+** | Website : https://mariosieg.com                                     |
+** | GitHub  : https://github.com/MarioSieg                              |
+** | License : https://www.apache.org/licenses/LICENSE-2.0               |
+** +---------------------------------------------------------------------+
+*/
+
+#pragma once
+
+namespace mag::bindings {
+
+}
