9db5051bb744ffa91417afa2b8abf41a64b67ddf
Mario Sieg <mario.sieg.64@gmail.com>
2026-06-11 17:09:56 +0000

    Add matmul contig info

--- magnetron/bindings/op_recorder.hpp
+++ magnetron/bindings/op_recorder.hpp
@@ -53,10 +53,13 @@ namespace mag::bindings {
       std::string dtype = sizeof...(Args) ? std::string{mag_type_trait(tensors[0]->dtype)->short_name} : "?";
       std::string kind {};
       if (opcode == MAG_OP_MATMUL && 2 == sizeof...(Args)) { // Matmul type for matmul
+        bool all_cont = mag_all_shapes_equal_and_contig(tensors.data(), tensors.size());
         kind = mag_matmul_type_name(mag_matmul_type_detect(tensors[0], tensors[1]));
+        kind += " ";
+        kind += all_cont ? "C" : "S";
       } else { // Else contig or strided kernel invocation
         bool all_cont = mag_all_shapes_equal_and_contig(tensors.data(), tensors.size());
-        kind = all_cont ? "contig" : "strided";
+        kind = all_cont ? "C" : "S";
       }
       auto start = std::chrono::high_resolution_clock::now();
       std::invoke(f);
