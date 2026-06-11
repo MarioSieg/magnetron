c44a3e0767319099f2a157294528e978fbdd49fb
Mario Sieg <mario.sieg.64@gmail.com>
2026-06-11 17:16:26 +0000

    Add matmul contig info

--- magnetron/cpu/kernels/mag_cpu_kernels_matmul_core.h
+++ magnetron/cpu/kernels/mag_cpu_kernels_matmul_core.h
@@ -13,7 +13,6 @@ static MAG_HOTPROC mag_status_t mag_matmul_generic(mag_error_t *err, const mag_k
   (void)err;
   mag_matmul_type_t type = mag_matmul_type_detect(payload->cmd->in[0], payload->cmd->in[1]);
   switch (type) {
-    case MAG_MATMUL_TYPE_INVALID: mag_contract(err, ERR_OPERATOR_IMPOSSIBLE, {}, type != MAG_MATMUL_TYPE_INVALID, "Invalid matmul type detected for the given input tensors"); break;
     case MAG_MATMUL_TYPE_DOT: mag_matmul_dot(payload); break;
     case MAG_MATMUL_TYPE_GEMV_VEC_MAT: mag_matmul_gemv_vec_mat(payload); break;
     case MAG_MATMUL_TYPE_GEMV_MAT_VEC: mag_matmul_gemv_mat_vec(payload); break;
@@ -21,8 +20,7 @@ static MAG_HOTPROC mag_status_t mag_matmul_generic(mag_error_t *err, const mag_k
     case MAG_MATMUL_TYPE_BMM_DOT:
     case MAG_MATMUL_TYPE_BMM_GEMV_VEC_MAT:
     case MAG_MATMUL_TYPE_BMM_GEMV_MAT_VEC:
-    case MAG_MATMUL_TYPE_BMM_GEMM:
-      mag_matmul_bmm(payload, type); break;
+    case MAG_MATMUL_TYPE_BMM_GEMM: mag_matmul_bmm(payload, type); break;
     default: mag_panic("NYI!");
   }
   return MAG_STATUS_OK;
@@ -33,7 +31,6 @@ static MAG_HOTPROC mag_status_t mag_matmul_fp8w_scaled(mag_error_t *err, const m
   (void)err;
   mag_matmul_type_t type = mag_matmul_type_detect(payload->cmd->in[0], payload->cmd->in[1]);
   switch (type) {
-    case MAG_MATMUL_TYPE_INVALID: mag_contract(err, ERR_OPERATOR_IMPOSSIBLE, {}, type != MAG_MATMUL_TYPE_INVALID, "Invalid matmul type detected for the given input tensors"); break;
     case MAG_MATMUL_TYPE_DOT: mag_matmul_fp8w_scaled_dot(payload); break;
     case MAG_MATMUL_TYPE_GEMV_VEC_MAT: mag_matmul_fp8w_scaled_gemv_vec_mat(payload); break;
     case MAG_MATMUL_TYPE_GEMV_MAT_VEC: mag_matmul_fp8w_scaled_gemv_mat_vec(payload); break;
