d6d4ea927a1a4dff9ae9a4248766edd652dd1c74
Mario Sieg <mario.sieg.64@gmail.com>
2026-06-25 20:28:57 -0700

    Socket fix

--- magnetron/core/mag_tcp_socket.c
+++ magnetron/core/mag_tcp_socket.c
@@ -80,33 +80,32 @@ bool mag_tcp_socket_accept(mag_tcp_socket_t *out_sock, mag_tcp_socket_t listener
 }
 
 bool mag_tcp_socket_connect(mag_tcp_socket_t *out_sock, const char *host, uint16_t port) {
-  mag_tcp_socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (mag_unlikely(!mag_tcp_socket_is_open(sock))) return false;
   struct sockaddr_in addr = {0};
   addr.sin_family = AF_INET;
   addr.sin_port = htons(port);
-  if (mag_unlikely(inet_pton(AF_INET, host, &addr.sin_addr)) != 1) {
-    mag_tcp_socket_close(sock);
+  if (mag_unlikely(inet_pton(AF_INET, host, &addr.sin_addr)) != 1)
     return false;
-  }
-  for (uint32_t retries=0;;) {
-    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == 0) break;
-    if (errno == EINTR) continue;
+  uint32_t retries=0;
+  for (;;) {
+    mag_tcp_socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (mag_unlikely(!mag_tcp_socket_is_open(sock)))
+      return false;
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == 0) {
+      if (mag_unlikely(!mag_tcp_socket_set_ops(sock))) {
+        mag_tcp_socket_close(sock);
+        return false;
+      }
+      *out_sock = sock;
+      return true;
+    }
+    int e = errno;
+    mag_tcp_socket_close(sock);
     if ((retries++ % 10) == 0) {
-      mag_log_level_t log_level_before = mag_log_level();
-      mag_set_log_level(MAG_LOG_LEVEL_INFO);
-      mag_log_info("Waiting for %s:%u (%s)\n", host, port, strerror(errno));
+      printf("[magnetron] Waiting for %s:%u (%s)\n", host, port, strerror(e));
       fflush(stdout);
-      mag_set_log_level(log_level_before);
     }
     mag_tcp_socket_sleep_ms(100);
   }
-  if (mag_unlikely(!mag_tcp_socket_set_ops(sock))) {
-    mag_tcp_socket_close(sock);
-    return false;
-  }
-  *out_sock = sock;
-  return true;
 }
 
 bool mag_tcp_socket_send_all(mag_tcp_socket_t sock, const void *buf, size_t nb) {
