d7f65c80602486ffa167753d6da001e52c3b30d5
Mario Sieg <mario.sieg.64@gmail.com>
2026-08-20 15:20:46 +0200

    Drop useless CPU backends

--- magnetron/cpu/amd64/amd64.cmake
+++ magnetron/cpu/amd64/amd64.cmake
@@ -13,14 +13,11 @@ mag_register_cpu_backend("amd64/mag_cpu_amd64_ivybridge.c"       "-march=ivybrid
 mag_register_cpu_backend("amd64/mag_cpu_amd64_nehalem.c"         "-march=nehalem -mtune=nehalem"                 "/arch:SSE4.2")  # SSE4.2
 mag_register_cpu_backend("amd64/mag_cpu_amd64_sandybridge.c"     "-march=sandybridge -mtune=sandybridge"         "/arch:AVX")    # first AVX
 mag_register_cpu_backend("amd64/mag_cpu_amd64_sapphirerapids.c"  "-march=sapphirerapids -mtune=sapphirerapids"   "/arch:AVX512") # AVX512-FP16; AMX via intrinsics
-mag_register_cpu_backend("amd64/mag_cpu_amd64_sierraforest.c"    "-march=sierraforest -mtune=sierraforest"       "/arch:AVX2")   # E-core server: AVX2 + AVX-VNNI
 mag_register_cpu_backend("amd64/mag_cpu_amd64_skylake_avx512.c"  "-march=skylake-avx512 -mtune=skylake-avx512"   "/arch:AVX512") # base AVX-512 (F+VL+DQ+BW)
 mag_register_cpu_backend("amd64/mag_cpu_amd64_tigerlake.c"       "-march=tigerlake -mtune=tigerlake"             "/arch:AVX512") # client AVX-512 (VNNI etc.)
-mag_register_cpu_backend("amd64/mag_cpu_amd64_westmere.c"        "-march=westmere -mtune=westmere"               "/arch:SSE4.2")  # SSE4.2 + PCLMUL
 
 # AMD
 mag_register_cpu_backend("amd64/mag_cpu_amd64_zn1.c"             "-march=znver1 -mtune=znver1"                   "/arch:AVX2")   # Zen/Zen+
-mag_register_cpu_backend("amd64/mag_cpu_amd64_zn2.c"             "-march=znver2 -mtune=znver2"                   "/arch:AVX2")
 mag_register_cpu_backend("amd64/mag_cpu_amd64_zn3.c"             "-march=znver3 -mtune=znver3"                   "/arch:AVX2")
 mag_register_cpu_backend("amd64/mag_cpu_amd64_zn4.c"             "-march=znver4 -mtune=znver4"                   "/arch:AVX512") # AVX-512 present on Zen 4 EPYC/Ryzen 7000
 mag_register_cpu_backend("amd64/mag_cpu_amd64_zn5.c"             "-march=znver5 -mtune=znver5"                   "/arch:AVX512") # AVX-VNNI + (AVX-512 on many SKUs)
